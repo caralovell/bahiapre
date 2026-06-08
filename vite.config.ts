3342f48e545af03475347dd40be3cccf3e32bf96
caralovell <168685711+caralovell@users.noreply.github.com>
2026-06-08 11:38:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/bahiapre/",
 
@@ -8,7 +14,9 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
